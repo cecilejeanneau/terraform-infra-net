d3b829d904bb45ff32dd8ab38c7cb7db2a6653e4
cecilejeanneau <cecile.n.jeanneau@gmail.com>
2026-04-14 14:28:26 +0200

    tp4: migrate terraform state to s3 backend

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 1.7.0"
 
+  backend "s3" {
+    bucket       = "tf-cecile-terraform-state-978514349126"
+    key          = "cecile/project.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
